be0e6ceeac0079a8d0c307fab64939744649edef
kenyaachon <swahili1963@gamil.com>
2022-06-25 00:02:53 -0700

    Recycling Script Variables

--- Project1/recycling.sh
+++ Project1/recycling.sh
@@ -9,14 +9,22 @@ TARFILE=/var/tmp/home_gichuhi.tar
 BZIPFILE=/var/tmp/home_gichuhi.tar.bz2
 SERVER=besancon
 REMOTEDIR=/opt/backup/franky
-LOGFILE=/home/gichuhi
+LOGFILE=/home/gichuhi/log/home_backup.log
 
 cd $BACKUPDIR
 
 #This create the archive
-tar cv $TARFILE $BACKUPFILES > /dev/null 2>&1
+tar cf $TARFILE $BACKUPFILES > /dev/null 2>&1 |sudo pv -s $(du -sb . | awk '{print $1}')
 
 #First remove theold bzip2 file
 #Redirect errors because this generates some if the archive does not exist
 rm $BZIPFILE 2> /dev/null
 bzip2 $TARFILE
+
+#Copy the file to another host - we have ssh keys for making this work without intervention
+#scp $BZIPFILE $SERVER:$REMOTEDIR > /dev/null 2>&1
+
+#create a timestamp in a logfile
+date >> $LOGFILE
+echo backup succeeded >> $LOGFILE
+
