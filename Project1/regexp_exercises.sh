3c5b02d87c6e11aad9080c4c8d42a49eaa58d10a
kenyaachon <swahili1963@gamil.com>
2022-07-22 22:39:37 -0700

    Exercises For RegExp

--- Project1/regexp_exercises.sh
+++ Project1/regexp_exercises.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+printf "Display a list of all the users on your system who log in with the Bash shell as a default. \n"
+
+printf "From the /etc/group directory, display all lines starting with the string 'daemon'. \n"
+
+printf "Print all the lines from the same file that don't contain the string."
+
+printf "Display localhost information from the /etc/hosts file, 
+display the line number(s) matching the search string and count the number of occurrences of the string.
+\n"
+
+printf "Display a list of /usr/share/doc subdirectories containing information about shells.\n"
+
+printf "How many README files do these subdirectories contain? Don't count anything in the form of 'README.a_string'.\n"
+
+printf "Make a list of files in your home directory that were changed less that 10 hours ago, using grep, but leave out directories. \n"
+
+printf "Put these commands in a shell script that will generate comprehensible output.\n"
+
+printf "Can you find an alternative for wc -l, using grep?\n"
+
+printf "Using the file system table (/etc/fstab for instance), list local disk devices.\n"
+
+printf "Make a script that checks whether a user exists in /etc/passwd. For now, you can specify the user name in the script, you don't have to work with arguments and conditionals at this stage.\n"
+
+printf "Display configuration files in /etc that contain numbers in their names.\n"
+
+
