d1bff881fbf20c8dc4ab3c68c26b4cf2abf70f80
kenyaachon <swahili1963@gamil.com>
2022-07-22 21:03:00 -0700

    Regular Expressions

---
+++ Project1/grep.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# using regular expressions
+# displays the lines from /etc/passwd containing
+printf "\n result for grep root /etc/passwd"
+grep root /etc/passwd
+
+# display the line numbers contain this search string
+printf "\n result for grep -n root /etc/passwd"
+grep -n root /etc/passwd
+
+# this check which users are not using bash, but accounts with the nologin shell are not displayed
+printf "\n result for grep -v bash /etc/passwd | grep -v nologin"
+grep -v bash /etc/passwd | grep -v nologin
+
+# display lines starting with the string "root"
+printf "\n result grep ^root /etc/passwd \n"
+grep ^root /etc/passwd
+
+# display which accounts have no shell assigned whatsoever
+printf "\n grep :$ /etc/passwd \n"
+grep :$ /etc/passwd
+
+# check that PATH is exported in ~/.bashrc, first select "export" lines and then
+# search for lines starting with the string "PATH", so as not to display
+# MANPATH and other possible paths
+printf "\n result for grep export ~/.bashrc | grep '\<PATH' \n"
+grep export ~/.bashrc | grep '\<PATH'
--- Project1/recycling.sh
+++ Project1/recycling.sh
@@ -3,28 +3,34 @@
 # This script makes a backup of home directory
 
 # Change the values of the variables to make the script work for you
-BACKUPDIR=/home
+BACKUPDIR=/home/gichuhi/Documents
 BACKUPFILES=gichuhi
 TARFILE=/var/tmp/home_gichuhi.tar
 BZIPFILE=/var/tmp/home_gichuhi.tar.bz2
 SERVER=besancon
 REMOTEDIR=/opt/backup/franky
 LOGFILE=/home/gichuhi/log/home_backup.log
+LOGDIR=/home/gichuhi/log
 
 cd $BACKUPDIR
 
 #This create the archive
-tar cf $TARFILE $BACKUPFILES > /dev/null 2>&1 |sudo pv -s $(du -sb . | awk '{print $1}')
-
+tar cf $TARFILE $BACKUPFILES > /dev/null 2>&1
 #First remove theold bzip2 file
 #Redirect errors because this generates some if the archive does not exist
+echo "Hello there my b"
 rm $BZIPFILE 2> /dev/null
 bzip2 $TARFILE
-
+echo "almost done"
 #Copy the file to another host - we have ssh keys for making this work without intervention
 #scp $BZIPFILE $SERVER:$REMOTEDIR > /dev/null 2>&1
 
 #create a timestamp in a logfile
+mkdir $LOGDIR
+touch $LOGFILE 
 date >> $LOGFILE
 echo backup succeeded >> $LOGFILE
+
+#reset the system
+rm -r $LOGDIR
 
